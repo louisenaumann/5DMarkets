4e683b054d0f595211a431ce77a6ed087fb30586
Louise Naumann <44986952+louisenaumann@users.noreply.github.com>
2026-06-30 23:56:58 +0200

    Update train_no_divergence.py

--- experiments/experiments/ablation/train_no_divergence.py
+++ experiments/experiments/ablation/train_no_divergence.py
@@ -15,7 +15,7 @@ import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 
-from src_torch.dataset import MarketSequenceDataset
+from src_torch.dataset2 import MarketSequenceDataset
 from no_divergence_model import NoDivergenceModel
 
 
