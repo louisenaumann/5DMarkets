4d7b4ec2722f9a1618596a48b5a05302c9a9c28c
Louise Naumann <44986952+louisenaumann@users.noreply.github.com>
2026-06-30 10:17:49 -0700

    Create plot_loss.py

---
+++ src_torch/plot_loss.py
@@ -0,0 +1,19 @@
+import matplotlib.pyplot as plt
+
+
+# paste your recorded losses here
+
+losses = [
+
+]
+
+
+plt.plot(losses)
+
+plt.xlabel("Epoch")
+
+plt.ylabel("Training Loss")
+
+plt.title("5DMarkets Training Curve")
+
+plt.show()
